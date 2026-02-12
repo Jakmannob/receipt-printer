adbf86ed50356d6375808dac5b2707bd07849694
Jakob Eigenmann <jakob.eigenmann@tum.de>
2026-02-12 19:41:54 +0100

    Some progress with the quiz and voice model

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # receipt-printer
+
 A collection of scripts I use for my receipt printer project
---
+++ quiz/parse_questions.py
@@ -0,0 +1,15 @@
+import json
+
+
+question_file = 'Quizfragen.md'
+question_output = 'quizfragen.json'
+questions = []
+with open(question_file, 'r') as infile:
+    for line in infile.readlines():
+        question = dict()
+        question['question'] = line.split('?')[0].strip() + '?'
+        question['answer'] = ('?'.join(line.split('?')[1:])).strip().lower()
+        questions.append(question)
+
+with open(question_output, 'w', encoding='utf8') as outfile:
+    json.dump(questions, outfile, sort_keys=True, indent=4, ensure_ascii=False)
